8749baf025207040c5757868d51fb51c471625f1
codeharshit-ty <hhsingh2011@gmail.com>
2025-08-26 18:11:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+num=23
+print("table of 23")
+for i in range(1,12):
+    mul= num*i
+    print("23 × %d=%d" % (i,mul))
+     
+print("even number are")
+for i in range(10,50):
+   if i%2==0:
+    print(f"{i} is even")
+       
+       
