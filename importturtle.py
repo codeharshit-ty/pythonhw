f1a51d143441c7c84fb2ae438f042d3eb3b2b7ea
codeharshit-ty <hhsingh2011@gmail.com>
2025-09-02 18:18:20 +0530

    Add files via upload

---
+++ importturtle.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+sc = turtle.Screen()
+sc.setup(400,300)
+turtle.title("welcome on turtle windows")
+board=turtle.Turtle()
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    
+    
