3104acb110f7d5247f3db6b1ecdac5aa9ab6a217
codeharshit-ty <hhsingh2011@gmail.com>
2025-10-18 14:32:40 +0530

    Add files via upload

---
+++ O(n)time.py
@@ -0,0 +1,10 @@
+def Ontime(n):
+    iteration = 0
+    for i in range(1,n+1):
+        iteration+=1
+    print ("when n is ", n,"iteration = ",iteration)
+
+Ontime(10)
+Ontime(20)
+Ontime(30)
+print("\nwith every 'n' the time taken and iterations will increase")
