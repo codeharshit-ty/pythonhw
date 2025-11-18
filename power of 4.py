a18ed2e764c389f0c61f5374b9f9f16d7ad5e643
codeharshit-ty <hhsingh2011@gmail.com>
2025-11-18 15:48:15 +0530

    Add files via upload

---
+++ power of 4.py
@@ -0,0 +1,15 @@
+def power4(number):
+    count =0
+    if ((number & (~(number-1))) == number):
+        while(number>1):
+            number>>=1
+            count+=1
+        if (count%2==0):
+            return True
+        else:
+            return False
+number = int(input("enter your number :"))
+if(power4(number)):
+    print(number,'is a power of 4')
+else:
+    print(number,'is not a power of 4')
