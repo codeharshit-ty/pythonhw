301adcc643d1f891d91573e053aa68db15db34a0
codeharshit-ty <hhsingh2011@gmail.com>
2025-11-20 13:27:05 +0530

    Add files via upload

---
+++ dividewithoutdivide.py
@@ -0,0 +1,14 @@
+def divide(ourDividend, ourDivisor):
+   sign = (-1 if ((ourDividend < 0) ^ 
+                (ourDivisor < 0)) else 1);
+   ourDividend = abs(ourDividend);
+   ourDivisor = abs(ourDivisor);
+   quotientNumber = 0
+   tempNumber = 0
+   for i in range(31, -1, -1):
+        if (tempNumber + (ourDivisor << i)) <= ourDividend:
+            tempNumber += ourDivisor << i
+            quotientNumber |= 1 << i
+
+   if sign == -1:
+     quotientNumber = -quotientNumber
