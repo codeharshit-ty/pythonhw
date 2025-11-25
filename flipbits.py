e41f9eb91feac0732bb75304db44fad4b6243386
codeharshit-ty <hhsingh2011@gmail.com>
2025-11-25 15:48:50 +0530

    Add files via upload

---
+++ flipbits.py
@@ -0,0 +1,13 @@
+def totalflips(number1,number2):
+    flips = 0
+    while(number1>0 or number2>0):
+        t1 = (number1&1)
+        t2 = (number2&1)
+        if(t1!=t2):
+            flips +=1
+        number1>>=1
+        number2>>=1
+    return flips
+number1 = int(input("enter the first number :"))
+number2 = int(input("enter the secound number :"))
+print("\nNumber of flips needed : ", totalflips(number1,number2))
